1934651fdce23fbb7a71e009e83cb9c35ce2e050
Tobias Marstaller <tobias.marstaller@gmail.com>
2017-02-14 01:40:16 +0100

    Refactored the Lexer to use the SourceFileReader

--- src/lexer/lexer.kt
+++ src/lexer/lexer.kt
@@ -1,42 +1,38 @@
 package lexer
 
+import transact.SourceFileReader
+import transact.TransactionalSequence
 import java.util.*
 
-class Lexer(private val input: List<Char>, private val sourceDescriptor: SourceDescriptor)
+class Lexer(code: String, private val sourceDescriptor: SourceDescriptor)
 {
-    /**
-     * This class works by performing "transactions" on the input list. The current location is stored in
-     * currentLocation; whenever an operation with unknown outcome is attempted, invoke mark(). That puts the
-     * current location onto a stack. If the operation succeeds, invoke commit(). That will pop that location
-     * off the stack again. If it fails, invoke resume(). That will reset the state to where it was when mark()
-     * was last invoked.
-     */
+    private val sourceTxSequence = SourceFileReader(code)
 
     /**
-     * @return The next token from the input or `null` if there are no more tokens.
+     * @return The nextToken token from the input or `null` if there are no more tokens.
      */
     @Synchronized
-    fun next(): Token?
+    fun nextToken(): Token?
     {
         skipWhitespace()
 
-        if (!hasNextChar()) return null
+        if (!hasNext) return null
 
         // try to match an operator
         for (operator in Operator.values())
         {
             val opToken = transact {
-                val nextText = nextChars(operator.text.length) ?: failTx()
+                val nextText = nextChars(operator.text.length) ?: rollback()
                 if (nextText == operator.text) {
                     return@transact OperatorToken(currentSL, operator)
                 }
-                failTx()
+                rollback()
             }
 
             if (opToken != null) return opToken
         }
 
-        if (!hasNextChar()) return null
+        if (!hasNext) return null
 
         // remaining possibilities: NUMERIC_LITERAL, KEYWORD, IDENTIFIER
         val text = collectWhile(Not(IsOperatorChar or IsWhitespace))!!
@@ -61,66 +57,14 @@ class Lexer(private val input: List<Char>, private val sourceDescriptor: SourceD
         }
     }
 
-    /** current location **/
-    private var currentLocation: LocationRef = LocationRef(1, 1, 0)
+    private val hasNext: Boolean
+        get() = sourceTxSequence.hasNext()
 
-    /**
-     * Returns the next character from the input and advances the input state; changes source line and column if
-     * necessary. **Invoke [mark] first if you are not 100% certain that you will use the return value for the current
-     * token.**
-     */
-    private fun nextChar(): Char?
-    {
-        if (input.lastIndex < currentLocation.inputIndex)
-        {
-            return null
-        }
+    private fun nextChar(): Char? = sourceTxSequence.next()
 
-        val char = input[currentLocation.inputIndex]
+    private fun nextChars(n: Int): String? = sourceTxSequence.next(n)
 
-        if (currentLocation.inputIndex > 0) {
-            var previousChar = input[currentLocation.inputIndex - 1]
-            if (previousChar == '\n')
-            {
-                currentLocation.sourceLine++
-                currentLocation.sourceColumn = 1
-            }
-            else
-            {
-                currentLocation.sourceColumn++
-            }
-        }
-
-        currentLocation.inputIndex++
-
-        return char
-    }
-
-    /**
-     * @return Whether an invocation of [nextChar] would yield a result.
-     */
-    private fun hasNextChar(): Boolean {
-        return input.lastIndex >= currentLocation.inputIndex
-    }
-
-    /**
-     * Returns the next [n] chars from the input as a string. Basically invokes [nextChar] [n] times and concatenates
-     * the results.
-     * @return The next [n] chars from the input or `null` if there are not enough chars left in the input.
-     */
-    private fun nextChars(n: Int): String?
-    {
-        if (currentLocation.inputIndex + n - 1 > input.lastIndex)
-        {
-            return null
-        }
-
-        val buf = StringBuilder(n)
-        for (i in 0..n - 1) {
-            buf.append(nextChar()!!)
-        }
-        return buf.toString()
-    }
+    private fun <T> transact(txCode: TransactionalSequence<Char, SourceFileReader.SourceLocation>.TransactionReceiver.() -> T): T? = sourceTxSequence.transact(txCode)
 
     /**
      * Collects chars from the input as long as they fulfill [pred]. If a character is encountered that does not
@@ -134,14 +78,14 @@ class Lexer(private val input: List<Char>, private val sourceDescriptor: SourceD
         var char: Char?
         while (true)
         {
-            mark()
-            char = nextChar()
+            sourceTxSequence.mark()
+            char = sourceTxSequence.next()
             if (char != null && pred(char)) {
                 buf.append(char)
-                commit()
+                sourceTxSequence.commit()
             }
             else {
-                resume()
+                sourceTxSequence.rollback()
                 break
             }
         }
@@ -154,81 +98,11 @@ class Lexer(private val input: List<Char>, private val sourceDescriptor: SourceD
         collectWhile(IsWhitespace)
     }
 
-    // here follows the transaction management code
-    private var markedPositions: Stack<LocationRef> = Stack()
-
-    /**
-     * Remembers the current location in the source. Use in conjunction with [commit] and [resume].
-     */
-    private fun mark()
-    {
-        markedPositions.push(currentLocation.copy())
-    }
-
-    /**
-     * Discards the last remembered location using [mark]. Invoke this method once you are sure that the characters
-     * you have consumed so fare will be returned in a token.
-     * @param nSteps The number of [mark] steps to erase.
-     */
-    private fun commit(nSteps: Int = 1)
-    {
-        for (i in 0..nSteps - 1) markedPositions.pop()
-    }
-
-    /**
-     * Resets the current source location to the last remembered location using [mark]. Use this if, after consuming
-     * some characters, it has turned out that they cannot be evaluated just yet.
-     * @param nSteps The number of [mark] steps to resume
-     */
-    private fun resume(nSteps: Int = 1) {
-        for (i in 0..nSteps - 1) currentLocation = markedPositions.pop()
-    }
-
-    /**
-     * Runs the given function within a "transaction" on the lexer state. If the function throws an exception,
-     * the state is reset and the exception is passed on. If the function returns gracefully, the result is passed on.
-     * @return The result of the given function or `null` if the function throws a [FailLexerTransactionException] (see [failTx]).
-     */
-    private fun <T> transact(txCode: Lexer.() -> T): T?
-    {
-        mark()
-        try
-        {
-            val retVal = this.txCode()
-            commit()
-            return retVal
-        }
-        catch (ex: Throwable)
-        {
-            resume()
-            if (ex is FailLexerTransactionException)
-            {
-                return null
-            }
-            else
-            {
-                throw ex
-            }
-        }
-    }
-
-    /**
-     * @throws FailLexerTransactionException Does nothing else; for use within [transact]
-     */
-    private fun failTx(): Nothing = throw FailLexerTransactionException()
-
     private val currentSL: SourceLocation
-        get() = sourceDescriptor.toLocation(currentLocation.sourceLine, currentLocation.sourceColumn)
-
-    private companion object {
-        class FailLexerTransactionException : Exception()
-
-        data class LocationRef(
-            var sourceLine: Int,
-            var sourceColumn: Int,
-            var inputIndex: Int
+        get() = sourceDescriptor.toLocation(
+                sourceTxSequence.currentPosition.lineNumber,
+                sourceTxSequence.currentPosition.columnNumber
         )
-    }
 }
 
 /**
@@ -236,7 +110,7 @@ class Lexer(private val input: List<Char>, private val sourceDescriptor: SourceD
  * source.
  */
 public fun lex(code: String, sD: SourceDescriptor): Sequence<Token> {
-    val lexer = Lexer(code.toCharArray().asList(), sD)
+    val lexer = Lexer(code, sD)
 
     return lexer.remainingTokens()
 }
@@ -247,7 +121,7 @@ public fun lex(code: String, sD: SourceDescriptor): Sequence<Token> {
 public fun lex(source: SourceContentAwareSourceDescriptor): Sequence<Token> {
     val code = source.sourceLines.joinToString("\n")
 
-    val lexer = Lexer(code.toCharArray().asList(), source)
+    val lexer = Lexer(code, source)
 
     return lexer.remainingTokens()
 }
@@ -260,7 +134,7 @@ public fun Lexer.remainingTokens(): Sequence<Token> {
 
     while (true)
     {
-        val token = next()
+        val token = nextToken()
         if (token != null)
         {
             tokens.add(token)
