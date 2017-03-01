4ed906505951fe5517c99dcd0f7806d2b6f06304
Tobias Marstaller <tobias.marstaller@gmail.com>
2017-03-01 01:33:29 +0100

    Adds a ROOT module with empty name
    - automagically includes all builtin types
    - all userland (including stdlib) module contexts will be derived from that ROOT context

--- src/compiler/ast/context/Module.kt
+++ src/compiler/ast/context/Module.kt
@@ -1,6 +1,8 @@
 package compiler.ast.context
 
 import compiler.ast.ModuleDeclaration
+import compiler.ast.type.BuiltinType
+import compiler.lexer.SourceLocation
 
 /**
  * A module, as declared by a source file.
@@ -8,4 +10,15 @@ import compiler.ast.ModuleDeclaration
 class Module(
     val declaration: ModuleDeclaration,
     val context: CTContext
-)
+) {
+    companion object {
+        /** The ROOT module to which all actual modules are added */
+        val ROOT: Module
+        init {
+            val rootDeclaration = ModuleDeclaration(SourceLocation.UNKNOWN, arrayOf())
+            val context = MutableCTContext()
+            context.include(BuiltinType.Context)
+            ROOT = Module(rootDeclaration, context)
+        }
+    }
+}
