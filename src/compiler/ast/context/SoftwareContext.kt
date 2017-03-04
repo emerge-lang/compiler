41803d6deeb799e51a9558f05146fa15e15b1d2a
Tobias Marstaller <tobias.marstaller@gmail.com>
2017-03-04 16:19:42 +0100

    SoftwareContext is now an authority for resolving FQNs
    Wiring modules together without a SoftwareContext should not be possible

--- src/compiler/ast/context/SoftwareContext.kt
+++ src/compiler/ast/context/SoftwareContext.kt
@@ -3,23 +3,27 @@ package compiler.ast.context
 import java.util.*
 
 /**
- * Just a bunch of modules with all their contents; all elements that make up a piece of software.
+ * Connects all elements that make up a piece of software. Is able to resolve fully-qualified names across
+ * contained [Module]s.
  */
 class SoftwareContext {
-    private val definedModules: MutableSet<Module> = HashSet()
+    /**
+     * All the modules. These don't nest!
+     */
+    private val modules: MutableSet<Module> = HashSet()
 
     /**
      * Looks for a module with the given name in this context. If none is defined, creates it.
      * @return The module with the given name within this context.
      */
     fun module(vararg name: String): Module {
-        val existing = definedModules.find { Arrays.equals(it.name, name) }
+        val existing = modules.find { Arrays.equals(it.name, name) }
         if (existing != null) {
             return existing
         }
 
         val module = Module(name, MutableCTContext())
-        this.definedModules.add(module)
+        this.modules.add(module)
         return module
     }
 }
