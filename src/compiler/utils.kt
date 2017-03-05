f30be0ed6ae6d09ba325916bde9c5fa9df7622c4
Tobias Marstaller <tobias.marstaller@gmail.com>
2017-03-05 19:02:19 +0100

    Added retryUntilNotNull property delegate

--- src/compiler/utils.kt
+++ src/compiler/utils.kt
@@ -7,6 +7,8 @@ import compiler.lexer.lex
 import compiler.parser.TokenSequence
 import java.nio.file.Path
 import java.nio.file.Paths
+import javax.naming.OperationNotSupportedException
+import kotlin.reflect.KProperty
 
 public fun parseFromClasspath(path: String): Module = parseFromClasspath(Paths.get(path))
 
@@ -22,4 +24,39 @@ public fun parseFromClasspath(path: Path): Module {
     matchResult.errors.forEach(::println)
 
     return matchResult.result ?: throw InternalCompilerError("Failed to parse from classpath $path")
+}
+
+/**
+ * Like [lazy] but keeps invoking the initializer until it returns a non-null value.
+ */
+fun <T> retryUntilNotNull(initializer: () -> T?) = RetriesUntilNotNull(initializer)
+
+fun <T> retryUntilNotNull(defaultValue: T, initializer: () -> T?) = RetriesUntilNotNullWithDefault(defaultValue, initializer)
+
+class RetriesUntilNotNull<T>(private val initializer: () -> T?) {
+    var value: T? = null
+
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): T? {
+        if (value != null) return value
+        value = initializer()
+        return value
+    }
+
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        throw OperationNotSupportedException()
+    }
+}
+
+class RetriesUntilNotNullWithDefault<T>(private val defaultValue: T, private val initializer: () -> T?) {
+    var value: T? = null
+
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        if (value != null) return defaultValue
+        value = initializer()
+        return value ?: defaultValue
+    }
+
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        throw OperationNotSupportedException()
+    }
 }
