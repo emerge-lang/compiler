6257b22e20f81a8fd5bccfbe6337a2562adbff52
Tobias Marstaller <tobias.marstaller@gmail.com>
2017-03-25 20:39:50 +0100

    Defines compiler.binding.BindingResult

---
+++ src/compiler/binding/BindingResult.kt
@@ -0,0 +1,20 @@
+package compiler.binding
+
+import compiler.parser.Reporting
+
+/**
+ * Binding parsed source to its (or a) [compiler.binding.context.CTContext] is supposed yield bound code
+ * (e.g. [compiler.ast.FunctionDeclaration] yields [Function]). Alongside that, the process can yield semantic
+ * [Reporting]s. This class models the result of a binding process (e.g. [compiler.ast.FunctionDeclaration.bindTo]).
+ */
+class BindingResult<out BoundType>(
+    /**
+     * The bound code. May be null if the semantic errors in the parsed code are so severe that
+     */
+    val bound: BoundType?,
+
+    /**
+     * [Reporting]s on semantic
+     */
+    val reportings: Collection<Reporting>
+)
