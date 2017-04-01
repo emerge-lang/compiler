ed7679a1ab0bb4bc7800147b4b23902a13f1f876
Tobias Marstaller <tobias.marstaller@gmail.com>
2017-04-01 23:24:10 +0200

    Refactored CodeChunk to the new bound AST type checking system

--- src/compiler/ast/CodeChunk.kt
+++ src/compiler/ast/CodeChunk.kt
@@ -1,25 +1,39 @@
 package compiler.ast
 
+import compiler.binding.BindingResult
+import compiler.binding.BoundCodeChunk
 import compiler.binding.context.CTContext
+import compiler.lexer.SourceLocation
 import compiler.parser.Reporting
 
 /**
  * A piece of executable code
  */
 class CodeChunk(
-    val statements: List<Executable>
-) : Executable {
-    override fun validate(context: CTContext): Collection<Reporting> {
+    val statements: List<Executable<*>>
+) : Executable<BoundCodeChunk> {
+    override val sourceLocation: SourceLocation = statements.firstOrNull()?.sourceLocation ?: SourceLocation.UNKNOWN
+
+    override fun bindTo(context: CTContext): BindingResult<BoundCodeChunk> {
         /** gets reassigned for every executable that modifies the context */
         var _context = context
 
         val reportings = mutableListOf<Reporting>()
 
-        statements.forEach { stmt ->
-            reportings.addAll(stmt.validate(_context))
-            _context = stmt.modified(_context)
+        val boundStmts = statements.map { stmt ->
+            val stmtBR = stmt.bindTo(_context)
+            reportings.addAll(stmtBR.reportings)
+            _context = stmtBR.bound.modified(_context)
+            stmtBR.bound
         }
 
-        return reportings
+        return BindingResult(
+            BoundCodeChunk(
+                context,
+                this,
+                boundStmts
+            ),
+            reportings
+        )
     }
 }
---
+++ src/compiler/binding/BoundCodeChunk.kt
@@ -0,0 +1,15 @@
+package compiler.binding
+
+import compiler.ast.CodeChunk
+import compiler.binding.context.CTContext
+
+class BoundCodeChunk(
+    /**
+     * Context that applies to the first statement; derivatives are stored within the statements themselves
+     */
+    override val context: CTContext,
+
+    override val declaration: CodeChunk,
+
+    val statements: List<BoundExecutable<*>>
+) : BoundExecutable<CodeChunk>
