6bd5a3c0cd305019450daf203a93be1edf5cf35f
Tobias Marstaller <tobias.marstaller@gmail.com>
2017-05-01 11:24:56 +0200

    Removed semantic analysis phase 1 from ReturnStatement
    ReturnStatement is just a wrapped expression. Expressions are no concern for phase 1.

--- src/compiler/binding/BoundReturnStatement.kt
+++ src/compiler/binding/BoundReturnStatement.kt
@@ -3,7 +3,6 @@ package compiler.binding
 import compiler.ast.ReturnStatement
 import compiler.binding.context.CTContext
 import compiler.binding.type.BaseTypeReference
-import compiler.parser.Reporting
 
 class BoundReturnStatement(
     override val context: CTContext,
@@ -13,12 +12,4 @@ class BoundReturnStatement(
 
     var returnType: BaseTypeReference? = null
         private set
-
-    fun semanticAnalysisPhase1(): Collection<Reporting> {
-        // invoke this method on the return statement
-        val reportings = expression.semanticAnalysisPhase1()
-        returnType = expression.type
-
-        return reportings
-    }
 }
