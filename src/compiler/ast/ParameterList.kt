09896226da9b52ac80b0ce1643369707065b45f3
Tobias Marstaller <tobias.marstaller@gmail.com>
2017-05-01 14:02:49 +0200

    Implements sematinc analysis phase 1 for ParameterList

--- src/compiler/ast/ParameterList.kt
+++ src/compiler/ast/ParameterList.kt
@@ -1,11 +1,8 @@
 package compiler.ast
 
 import compiler.ast.type.TypeReference
-import compiler.binding.BindingResult
-import compiler.binding.BoundParameter
 import compiler.binding.BoundParameterList
 import compiler.binding.context.CTContext
-import compiler.parser.Reporting
 
 class ParameterList (
     val parameters: List<VariableDeclaration> = emptyList()
@@ -13,39 +10,9 @@ class ParameterList (
     /** The types; null values indicate non-specified parameters */
     val types: List<TypeReference?> = parameters.map { it.type }
 
-    fun bindTo(context: CTContext, allowUntyped: Boolean = true): BindingResult<BoundParameterList> {
-        val reportings = mutableListOf<Reporting>()
-
-        val boundParams = mutableListOf<BoundParameter>()
-        parameters.forEach { param ->
-            val boundParamResult = param.bindTo(context)
-            val boundParam = boundParamResult.bound
-
-            // double names
-            if (boundParams.find { it.name == boundParam.name } != null) {
-                reportings.add(Reporting.error("BoundParameter ${param.name.value} is already defined in the parameter list", param.name))
-            }
-            else {
-                boundParams.add(boundParam)
-            }
-
-            if (!allowUntyped && param.type == null) {
-                reportings.add(Reporting.error("The type of parameter ${param.name.value} must be explicitly declared.", param.declaredAt))
-            }
-
-            // etc.
-            reportings.addAll(boundParamResult.reportings)
-        }
-
-        return BindingResult(
-            BoundParameterList(
-                context,
-                this,
-                boundParams
-            ),
-            reportings
-        )
-    }
-
-    override fun bindTo(context: CTContext) = bindTo(context, false)
+    override fun bindTo(context: CTContext) = BoundParameterList(
+        context,
+        this,
+        parameters.map { it.bindTo(context) }
+    )
 }
--- src/compiler/binding/BoundParameterList.kt
+++ src/compiler/binding/BoundParameterList.kt
@@ -2,11 +2,32 @@ package compiler.binding
 
 import compiler.ast.ParameterList
 import compiler.binding.context.CTContext
+import compiler.parser.Reporting
 
 class BoundParameterList(
     val context: CTContext,
     val declaration: ParameterList,
     val parameters: List<BoundParameter>
-)
+) {
+    fun semanticAnalysisPhase1(allowUntyped: Boolean = true): Collection<Reporting> {
+        val reportings = mutableListOf<Reporting>()
+
+        // double names
+        for (param in parameters) {
+            if (parameters.find { it !== param && it.name == param.name } != null) {
+                reportings.add(Reporting.error("BoundParameter ${param.name} is already defined in the parameter list", param.declaration.declaredAt))
+            }
+
+            if (!allowUntyped && param.declaration.type == null) {
+                reportings.add(Reporting.error("The type of parameter ${param.name} must be explicitly declared.", param.declaration.declaredAt))
+            }
+
+            // etc.
+            reportings.addAll(param.semanticAnalysisPhase1("parameter"))
+        }
+
+        return reportings
+    }
+}
 
 typealias BoundParameter = BoundVariable
