04e528645c02e079bdd98ae0c44d0cf65fe8ae6d
Tobias Marstaller <tobias.marstaller@gmail.com>
2017-05-12 18:53:04 +0200

    WIP refactoring InvocationExpression

--- src/compiler/ast/expression/InvocationExpression.kt
+++ src/compiler/ast/expression/InvocationExpression.kt
@@ -18,125 +18,38 @@ class InvocationExpression(
      * * `doStuff()` => `IdentifierExpression(doStuff)`
      * * `obj.doStuff()` => `MemberAccessExpression(obj, doStuff)`
      */
-    val functionExpression: Expression<*>,
+    val targetExpression: Expression<*>,
     val parameterExprs: List<Expression<*>>
 ) : Expression<BoundInvocationExpression>, Executable<BoundInvocationExpression> {
-    override val sourceLocation: SourceLocation = when(functionExpression) {
-        is MemberAccessExpression -> functionExpression.memberName.sourceLocation
-        else -> functionExpression.sourceLocation
+    override val sourceLocation: SourceLocation = when(targetExpression) {
+        is MemberAccessExpression -> targetExpression.memberName.sourceLocation
+        else -> targetExpression.sourceLocation
     }
 
-    override fun bindTo(context: CTContext): BindingResult<BoundInvocationExpression> {
+    override fun bindTo(context: CTContext): BoundInvocationExpression {
         val reportings = mutableListOf<Reporting>()
 
         // bind all the parameters
-        val boundParameterValueExprs = parameterExprs.map {
-            val br = it.bindTo(context)
-            reportings.addAll(br.reportings)
-            br.bound
-        }
+        val boundParameterValueExprs = parameterExprs.map { it.bindTo(context) }
 
-        // decide the kind of dispatch
-        if (functionExpression is MemberAccessExpression) {
-            val receiverExprBR = functionExpression.valueExpression.bindTo(context)
-            reportings.addAll(receiverExprBR.reportings)
-
-            // TODO: member functions
-
-            return bindTo_staticDispatch_withReceiver(
-                context,
-                receiverExprBR.bound,
-                functionExpression.memberName.value,
-                boundParameterValueExprs,
-                reportings
+        if (targetExpression is MemberAccessExpression) {
+            return BoundInvocationExpression(
+                    context,
+                    this,
+                    targetExpression.valueExpression.bindTo(context),
+                    targetExpression.memberName,
+                    boundParameterValueExprs
             )
         }
-        else if (functionExpression is IdentifierExpression) {
-            // TODO: function types => variable of function type
-
-            return bindTo_staticDispatch_withoutReceiver(
-                context,
-                functionExpression.identifier.value,
-                boundParameterValueExprs,
-                reportings
+        else if (targetExpression is IdentifierExpression) {
+            return BoundInvocationExpression(
+                    context,
+                    this,
+                    null,
+                    targetExpression.identifier,
+                    boundParameterValueExprs
             )
         }
         else throw InternalCompilerError("What the heck is going on?? The parser should never have allowed this!")
-    }
-
-    private fun bindTo_staticDispatch_withoutReceiver(
-        context: CTContext,
-        functionName: String,
-        boundParameterValueExprs: List<BoundExpression<*>>,
-        reportings: MutableCollection<Reporting>
-    ): BindingResult<StaticDispatchInvocationExpression>
-    {
-        // attempt to resolve standalone function
-        val functions = context.resolveAnyFunctions(functionName)
-            .filter { it.receiverType == null }
-
-        if (functions.isEmpty()) {
-            reportings.add(Reporting.error("Function $functionName is not defined", sourceLocation))
-        }
-
-        val bestMatch = functions
-            .filterAndSortByMatchForInvocationTypes(null, boundParameterValueExprs.map(BoundExpression<*>::type))
-            .firstOrNull()
-
-        return BindingResult(
-            StaticDispatchInvocationExpression(
-                context,
-                this,
-                functionName,
-                bestMatch,
-                BoundNullLiteralExpression.getInstance(context, sourceLocation),
-                boundParameterValueExprs
-            ),
-            reportings
-        )
-    }
-
-    private fun bindTo_staticDispatch_withReceiver(
-        context: CTContext,
-        receiverExpr: BoundExpression<*>,
-        functionName: String,
-        boundParameterValueExprs: List<BoundExpression<*>>,
-        reportings: MutableCollection<Reporting>
-    ): BindingResult<StaticDispatchInvocationExpression>
-    {
-        val receiverType = receiverExpr.type ?: compiler.binding.type.Any.baseReference(context).nullable()
-
-        // attempt to resolve extension function
-        val functionsByName = context.resolveAnyFunctions(functionName)
-
-        if (functionsByName.isEmpty()) {
-            reportings.add(Reporting.error("Function $functionName is not defined", sourceLocation))
-        }
-
-        val forReceiverType = functionsByName
-            .filter { it.receiverType != null }
-            .filter { receiverType isAssignableTo it.receiverType!! }
-
-        // receiverType is genuine and no functions are found; this is an error
-        // otherwise, the error would be consecutive
-        if (forReceiverType.isEmpty() && receiverExpr.type != null) {
-            reportings.add(Reporting.error("Function $functionName is not defined on type $receiverType", sourceLocation))
-        }
-
-        val bestMatch = forReceiverType
-            .filterAndSortByMatchForInvocationTypes(receiverType, boundParameterValueExprs.map(BoundExpression<*>::type))
-            .firstOrNull()
-
-        return BindingResult(
-            StaticDispatchInvocationExpression(
-                context,
-                this,
-                functionName,
-                bestMatch,
-                receiverExpr,
-                boundParameterValueExprs
-            ),
-            reportings
-        )
     }
 }
--- src/compiler/binding/expression/BoundInvocationExpression.kt
+++ src/compiler/binding/expression/BoundInvocationExpression.kt
@@ -1,38 +1,122 @@
 package compiler.binding.expression
 
 import compiler.ast.expression.InvocationExpression
-import compiler.binding.BoundExecutable
-import compiler.binding.BoundFunction
 import compiler.binding.context.CTContext
+import compiler.binding.filterAndSortByMatchForInvocationTypes
+import compiler.binding.type.BaseTypeReference
+import compiler.lexer.IdentifierToken
+import compiler.parser.Reporting
 
-abstract class BoundInvocationExpression : BoundExpression<InvocationExpression>, BoundExecutable<InvocationExpression>
-
-/**
- * Statically dispatched invocation: the target jump address of the call is known at compile-time.
- *
- * Note that static dispatch can result from other kinds of invocation, too (e.g. dynamic dispatch where, at compile time,
- * it is known that the function is never overridden).
- */
-class StaticDispatchInvocationExpression(
+class BoundInvocationExpression(
     override val context: CTContext,
     override val declaration: InvocationExpression,
-    val functionName: String,
+    /** The receiver expression; is null if not specified in the source */
+    val receiverExpression: BoundExpression<*>?,
+    val functionNameToken: IdentifierToken,
+    val parameterExpressions: List<BoundExpression<*>>
+) : BoundExpression<InvocationExpression> {
+    override var type: BaseTypeReference? = null
+        private set
 
-    /**
-     * The invoked function. Is null if it could not be inferred.
-     */
-    val function: BoundFunction?,
+    fun semanticAnalysisPhase1(): Collection<Reporting> {
+        // TODO: call semantic analysis on all sub-expression (catch typos in casts)
+        return emptySet()
+    }
 
-    /**
-     * Receiver parameter of the function. This variable is `null` if the invoked function has no receiver
-     * and the invocation was in a context without receiver.
-     */
-    val receiverValue: BoundExpression<*>?,
+    fun semanticAnalysisPhase2(): Collection<Reporting> {
+        // TODO: invoke phase 2 on all sub-expressions
 
-    /**
-     * The values of the parameters, in order.
-     */
-    val parameterValues: List<BoundExpression<*>>
-) : BoundInvocationExpression() {
-    override val type = function?.returnType
+        // TODO: determine the function to be invoked.
+        // in case of ambiguity: report an error
+
+        // TODO: determine evaluation type from the resolved function to invoke
+        // in case of ambiguity: do not attempt to infer or guess the return type.
+        this.type = compiler.binding.type.Any.baseReference(context)
+
+        // TODO: determine type of invocation: static dispatch or dynamic dispatch
+
+        return emptySet()
+    }
+
+    fun semanticAnalysisPhase3(): Collection<Reporting> {
+        // TODO: invoke semantic analysis on all sub-expressions
+
+        return emptySet()
+    }
+
+    private fun bindTo_staticDispatch_withoutReceiver(
+            context: CTContext,
+            functionName: String,
+            boundParameterValueExprs: List<BoundExpression<*>>,
+            reportings: MutableCollection<Reporting>
+    ): BindingResult<StaticDispatchInvocationExpression>
+    {
+        // attempt to resolve standalone function
+        val functions = context.resolveAnyFunctions(functionName)
+                .filter { it.receiverType == null }
+
+        if (functions.isEmpty()) {
+            reportings.add(Reporting.error("Function $functionName is not defined", sourceLocation))
+        }
+
+        val bestMatch = functions
+                .filterAndSortByMatchForInvocationTypes(null, boundParameterValueExprs.map(BoundExpression<*>::type))
+                .firstOrNull()
+
+        return BindingResult(
+                StaticDispatchInvocationExpression(
+                        context,
+                        this,
+                        functionName,
+                        bestMatch,
+                        BoundNullLiteralExpression.getInstance(context, sourceLocation),
+                        boundParameterValueExprs
+                ),
+                reportings
+        )
+    }
+
+    private fun bindTo_staticDispatch_withReceiver(
+            context: CTContext,
+            receiverExpr: BoundExpression<*>,
+            functionName: String,
+            boundParameterValueExprs: List<BoundExpression<*>>,
+            reportings: MutableCollection<Reporting>
+    ): BindingResult<StaticDispatchInvocationExpression>
+    {
+        val receiverType = receiverExpr.type ?: compiler.binding.type.Any.baseReference(context).nullable()
+
+        // attempt to resolve extension function
+        val functionsByName = context.resolveAnyFunctions(functionName)
+
+        if (functionsByName.isEmpty()) {
+            reportings.add(Reporting.error("Function $functionName is not defined", sourceLocation))
+        }
+
+        val forReceiverType = functionsByName
+                .filter { it.receiverType != null }
+                .filter { receiverType isAssignableTo it.receiverType!! }
+
+        // receiverType is genuine and no functions are found; this is an error
+        // otherwise, the error would be consecutive
+        if (forReceiverType.isEmpty() && receiverExpr.type != null) {
+            reportings.add(Reporting.error("Function $functionName is not defined on type $receiverType", sourceLocation))
+        }
+
+        val bestMatch = forReceiverType
+                .filterAndSortByMatchForInvocationTypes(receiverType, boundParameterValueExprs.map(BoundExpression<*>::type))
+                .firstOrNull()
+
+        return BindingResult(
+                StaticDispatchInvocationExpression(
+                        context,
+                        this,
+                        functionName,
+                        bestMatch,
+                        receiverExpr,
+                        boundParameterValueExprs
+                ),
+                reportings
+        )
+    }
 }
