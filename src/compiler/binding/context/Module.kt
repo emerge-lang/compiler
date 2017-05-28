a602ae6b1796ce38c921c1ae02b7e9bb2d99c725
Tobias Marstaller <tobias.marstaller@gmail.com>
2017-05-28 14:45:59 +0200

    Declares semantic analysis methods on module

--- src/compiler/binding/context/Module.kt
+++ src/compiler/binding/context/Module.kt
@@ -1,6 +1,36 @@
 package compiler.binding.context
 
+import compiler.binding.BoundFunction
+import compiler.binding.BoundVariable
+import compiler.parser.Reporting
+
 class Module(
     val name: Array<String>,
-    val context: MutableCTContext
-)
+    val context: MutableCTContext,
+    /** [Reporting]s generated at bind-time: double declarations, ... */
+    val bindTimeReportings: Collection<Reporting> = emptySet()
+) {
+    /**
+     * Delegates to semantic analysis phase 1 of all components that make up this module;
+     * collects the results and returns them. Also returns the [Reporting]s found when binding
+     * elements to the module (such as doubly declared variables).
+     */
+    fun semanticAnalysisPhase1(): Collection<Reporting> =
+        bindTimeReportings +
+        context.variables.flatMap(BoundVariable::semanticAnalysisPhase1) +
+        context.functions.flatMap(BoundFunction::semanticAnalysisPhase1)
+
+    /**
+     * Delegates to semantic analysis phase 2 of all components that make up this module;
+     * collects the results and returns them.
+     */
+    fun semanticAnalysisPhase2(): Collection<Reporting> =
+        context.variables.flatMap(BoundVariable::semanticAnalysisPhase2) +
+        context.functions.flatMap(BoundFunction::semanticAnalysisPhase2)
+
+    /**
+     * Delegates to semantic analysis phase 3 of all components that make up this module;
+     * collects the results and returns them.
+     */
+    fun semanticAnalysisPhase3(): Collection<Reporting> = emptySet() // TODO: implement
+}
