6fd37ffbd438f870e98ed89575b10d5ee5af4c06
Tobias Marstaller <tobias.marstaller@gmail.com>
2017-06-28 15:54:48 +0200

    BoundIdentifierExpression now publishes the type of thing it refers to (variable, type, ...)

--- src/compiler/binding/expression/BoundIdentifierExpression.kt
+++ src/compiler/binding/expression/BoundIdentifierExpression.kt
@@ -1,6 +1,7 @@
 package compiler.binding.expression
 
 import compiler.ast.expression.IdentifierExpression
+import compiler.binding.BoundVariable
 import compiler.binding.context.CTContext
 import compiler.binding.type.BaseTypeReference
 import compiler.parser.Reporting
@@ -12,6 +13,14 @@ class BoundIdentifierExpression(
     val identifier: String = declaration.identifier.value
 
     override var type: BaseTypeReference? = null
+        private set
+
+    /** What this expression refers to; is null if not known */
+    var referredType: ReferredType? = null
+        private set
+
+    /** The variable this expression refers to, if it does (see [referredType]); otherwise null. */
+    var referredVariable: BoundVariable? = null
         private set
 
     /**
@@ -28,7 +37,8 @@ class BoundIdentifierExpression(
         val variable = context.resolveVariable(identifier)
         if (variable != null) {
             type = variable.type
-            // TODO: check access modifier
+            referredVariable = variable
+            referredType = ReferredType.VARIABLE
         }
         else {
             reportings.add(Reporting.error("Cannot resolve variable $identifier", declaration.sourceLocation))
@@ -37,5 +47,11 @@ class BoundIdentifierExpression(
         // TODO: attempt to resolve type; expression becomes of type "Type/Class", ... whatever, still to be defined
 
         return reportings
+    }
+
+    /** The kinds of things an identifier can refer to. */
+    enum class ReferredType {
+        VARIABLE,
+        TYPENAME
     }
 }
