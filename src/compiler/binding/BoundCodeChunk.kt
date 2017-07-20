b1581eb33b2ef831e0648b6b00f9bcd5de3cb889
Tobias Marstaller <tobias.marstaller@gmail.com>
2017-07-20 16:36:52 +0200

    BoundCodeChunk: semantic analysis phase 3: process the statements

--- src/compiler/binding/BoundCodeChunk.kt
+++ src/compiler/binding/BoundCodeChunk.kt
@@ -2,6 +2,7 @@ package compiler.binding
 
 import compiler.ast.CodeChunk
 import compiler.binding.context.CTContext
+import compiler.parser.Reporting
 
 class BoundCodeChunk(
     /**
@@ -11,6 +12,32 @@ class BoundCodeChunk(
 
     override val declaration: CodeChunk
 ) : BoundExecutable<CodeChunk> {
+
+    /** The bound statements of this code; must not be null after semantic analysis is done */
+    var statements: List<BoundExecutable<*>>? = null
+        private set
+
     override var isReadonly: Boolean? = null
         private set
+
+    override fun semanticAnalysisPhase3(): Collection<Reporting> {
+        val reportings = mutableSetOf<Reporting>()
+        var currentContext = context
+        val boundStatements = mutableListOf<BoundExecutable<*>>()
+
+        for (astStatement in declaration.statements) {
+            val boundStatement = astStatement.bindTo(currentContext)
+
+            reportings += boundStatement.semanticAnalysisPhase1()
+            reportings += boundStatement.semanticAnalysisPhase2()
+            reportings += boundStatement.semanticAnalysisPhase3()
+
+            boundStatements.add(boundStatement)
+            currentContext = boundStatement.modified(currentContext)
+        }
+
+        this.statements = boundStatements
+
+        return reportings
+    }
 }
