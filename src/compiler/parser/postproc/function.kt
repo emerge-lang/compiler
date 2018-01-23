e6a77bc930c5178d4db4df03eda1ea944e693dd1
Tobias Marstaller <tobias.marstaller@gmail.com>
2018-01-23 20:54:34 +0100

    Added copyright notice to all files

--- src/compiler/parser/postproc/function.kt
+++ src/compiler/parser/postproc/function.kt
@@ -1,17 +1,36 @@
+/*
+ * Copyright 2018 Tobias Marstaller
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
 package compiler.parser.postproc
 
 import compiler.InternalCompilerError
-import compiler.ast.*
+import compiler.ast.CodeChunk
+import compiler.ast.FunctionDeclaration
+import compiler.ast.ParameterList
+import compiler.ast.ReturnStatement
 import compiler.ast.expression.Expression
 import compiler.ast.type.FunctionModifier
 import compiler.ast.type.TypeReference
-import compiler.binding.type.Unit
 import compiler.lexer.*
 import compiler.parser.rule.Rule
 import compiler.parser.rule.RuleMatchingResult
 import compiler.transact.Position
 import compiler.transact.TransactionalSequence
-import java.util.*
 
 fun StandaloneFunctionPostprocessor(rule: Rule<List<RuleMatchingResult<*>>>): Rule<FunctionDeclaration> {
     return rule
