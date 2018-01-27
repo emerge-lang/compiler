441a155f80d033a812814c2db62f15bdf77f77e4
Tobias Marstaller <tobias.marstaller@gmail.com>
2018-01-27 13:18:04 +0100

    Test Suit: provides facility to mock lexer in grammar tests

--- test/compiler/parser/grammar/GrammarTestCase.kt
+++ test/compiler/parser/grammar/GrammarTestCase.kt
@@ -18,8 +18,10 @@
 
 package compiler.parser.grammar
 
-import compiler.lexer.SourceContentAwareSourceDescriptor
+import compiler.lexer.*
 import compiler.parser.TokenSequence
+import compiler.parser.rule.Rule
+import compiler.parser.rule.RuleMatchingResult
 import compiler.parser.toTransactional
 import io.kotlintest.specs.FreeSpec
 
@@ -32,4 +34,91 @@ abstract class GrammarTestCase : FreeSpec() {
 
         return compiler.lexer.lex(code, source).toTransactional(source.toLocation(1, 1))
     }
+
+    fun tokenSequence(sequenceDefinition: TokenSequenceGenerator.() -> Unit): TokenSequence {
+        val generator = TokenSequenceGenerator()
+        generator.sequenceDefinition()
+        return generator.toTransactional()
+    }
+
+    fun <R> TokenSequence.matchAgainst(rule: Rule<R>): RuleMatchingResult<R> = rule.tryMatch(this)
+}
+
+class TokenSequenceGenerator {
+
+    private val tokens: MutableList<Token> = ArrayList()
+
+    private var currentLine = 1
+    private var currentColumn = 1
+    private val locationModificationMutex = Any()
+
+    private val sD = SimpleSourceDescriptor("mocked tokens")
+    private val initialSourceLocation = SourceLocation(sD, 1, 1)
+
+    fun toTransactional(): TokenSequence {
+        return TokenSequence(ArrayList(tokens), initialSourceLocation)
+    }
+
+    fun identifier(value: String) {
+        var location: SourceLocation? = null
+
+        synchronized(locationModificationMutex) {
+            location = SourceLocation(sD, currentLine, currentColumn)
+            currentColumn += value.length + 1
+        }
+
+        tokens += IdentifierToken(value, location!!)
+    }
+
+    fun keyword(keyword: Keyword) {
+        var location: SourceLocation? = null
+
+        synchronized(locationModificationMutex) {
+            location = SourceLocation(sD, currentLine, currentColumn)
+            currentColumn += keyword.text.length + 1
+        }
+
+        tokens += KeywordToken(keyword, keyword.text, location!!)
+    }
+
+    fun operator(operator: Operator) {
+        var location: SourceLocation? = null
+
+        synchronized(locationModificationMutex) {
+            location = SourceLocation(sD, currentLine, currentColumn)
+
+            if (operator == Operator.NEWLINE) {
+                currentLine++
+                currentColumn = 1
+            } else {
+                currentColumn += operator.text.length + 1
+            }
+        }
+
+        tokens += OperatorToken(operator, location!!)
+    }
+
+    fun newline() {
+        operator(Operator.NEWLINE)
+    }
+
+    fun mockRef(replacement: Any) {
+        var location: SourceLocation? = null
+
+        synchronized(locationModificationMutex) {
+            location = SourceLocation(sD, currentLine, currentColumn)
+            currentColumn += 2
+        }
+
+        tokens += NestedRuleMockingToken(replacement, location!!)
+    }
+}
+
+/**
+ * To be used as a placeholder for nested rules. E.g. the rule [ParameterList] uses [GrammarReceiver.ref] with
+ * [VariableDeclaration] as the parameter. An instance of this class can be used instead of actual tokens resembling
+ * the variable declaration. The given [replacement] will be returned as if the nested rule had parsed it.
+ */
+class NestedRuleMockingToken(val replacement: Any, override val sourceLocation: SourceLocation) : Token() {
+    override val type: TokenType = TokenType.OPERATOR
 }
