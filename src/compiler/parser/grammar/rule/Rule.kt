9098dfebb7f0c0718b74ecfb6a0f1ea0c7ccd11c
Tobias <tobias.marstaller@gmail.com>
2023-11-22 00:57:33 +0100

    move SingleTokenRule and ExpectedToken into their own files

--- src/compiler/parser/grammar/rule/Rule.kt
+++ src/compiler/parser/grammar/rule/Rule.kt
@@ -18,10 +18,7 @@
 
 package compiler.parser.grammar.rule
 
-import compiler.lexer.Token
 import compiler.parser.TokenSequence
-import compiler.parser.grammar.dsl.GrammarReceiver
-import compiler.reportings.Reporting
 
 interface Rule<T> {
     val descriptionOfAMatchingThing: String
@@ -38,113 +35,4 @@ interface Rule<T> {
     val minimalMatchingSequence: Sequence<Sequence<ExpectedToken>>
 
     fun markAmbiguityResolved(inContext: Any)
-}
-
-sealed class SingleTokenRule(
-    private val expectedToken: ExpectedToken,
-) : Rule<Token> {
-    override val explicitName = null
-
-    private val marksEndOfAmbiguityInContexts = HashSet<Any>()
-
-    final override val minimalMatchingSequence: Sequence<Sequence<ExpectedToken>> by lazy {
-        sequenceOf(sequenceOf(MarkingExpectedToken(expectedToken)))
-    }
-
-    final override fun tryMatch(context: Any, input: TokenSequence): RuleMatchingResult<Token> {
-        if (!input.hasNext()) {
-            return RuleMatchingResult(
-                isAmbiguous = true,
-                marksEndOfAmbiguity = false,
-                item = null,
-                reportings = setOf(Reporting.unexpectedEOI(descriptionOfAMatchingThing, input.currentSourceLocation))
-            )
-        }
-
-        input.mark()
-
-        val token = input.next()!!
-        val processed = matchAndPostprocess(token)
-        if (processed != null) {
-            input.commit()
-            return RuleMatchingResult(
-                isAmbiguous = false,
-                marksEndOfAmbiguity = context in marksEndOfAmbiguityInContexts,
-                item = processed,
-                reportings = emptySet(),
-            )
-        }
-
-        input.rollback()
-        return RuleMatchingResult(
-            isAmbiguous = true,
-            marksEndOfAmbiguity = false,
-            item = null,
-            setOf(Reporting.mismatch(descriptionOfAMatchingThing, token)),
-        )
-    }
-
-    override fun markAmbiguityResolved(inContext: Any) {
-        // nothing to do, no nested rules to inform and no own bookkeeping to adjust
-    }
-
-    /**
-     * @return if matched, the token, possibly modified (e.g. [IdentifierRule]). `null` on mismatch
-     */
-    abstract fun matchAndPostprocess(token: Token): Token?
-
-    override fun toString() = "single token: $descriptionOfAMatchingThing"
-
-    private inner class MarkingExpectedToken(
-        private val delegate: ExpectedToken,
-    ) : ExpectedToken {
-        override fun markAsRemovingAmbiguity(inContext: Any) {
-            marksEndOfAmbiguityInContexts.add(inContext)
-            delegate.markAsRemovingAmbiguity(inContext)
-        }
-        override fun unwrap() = delegate
-        override fun toString() = delegate.toString()
-    }
-}
-
-/**
- * A placeholder for a matching token, e.g. an identifier, an operator, ...
- *
- * When implementing this interface for a singular token (e.g. [GrammarReceiver.identifier]) you
- * likely don't need to implement any of the methods. A good [toString] greatly helps debugging!
- *
- * If your implementation wraps another instance of [ExpectedToken] you need to be very careful with which
- * methods you override and how you do it. At least [markAsRemovingAmbiguity] and [unwrap] are a necessity then,
- * and [toString] should delegate to the wrapped instance.
- */
-interface ExpectedToken {
-    /**
-     * Called on the first [ExpectedToken] in a sequence returned from [Rule.minimalMatchingSequence]
-     * which is unique among the other options (see e.g. [EitherOfRule]). By consequence, once this
-     * token is matched in any invocation of [Rule.tryMatch], the [RuleMatchingResult.isAmbiguous] returned
-     * from then on must be `true`.
-     * All of that is scoped to each unique context ([inContext]).
-     */
-    fun markAsRemovingAmbiguity(inContext: Any) {}
-
-    /**
-     * @return an [ExpectedToken] that directly models an expected token, rather than delegating/wrapping
-     * another [ExpectedToken] instance. May return itself if that's already the case.
-     */
-    fun unwrap(): ExpectedToken = this
-
-    /**
-     * @return true Iff a [Token] exists that would match both `this` and [other].
-     */
-    fun couldMatchSameTokenAs(other: ExpectedToken): Boolean = unwrap().couldMatchSameTokenAs(other.unwrap())
-
-    /**
-     * has the same contract as [Any.equals].
-     *
-     * `a.isCloneOf(b)` implies `a.couldMatchSameTokenAs(b)`
-     *
-     * @return whether [this] and [other] refer to the same original token in the same grammar, through the
-     * same route. `a.isCloneOf(b) && a !== b` happens to tokens preceding an [EitherOfRule] in a [SequenceRule].
-     */
-    fun isCloneOf(other: ExpectedToken): Boolean = this === other
 }
---
+++ src/compiler/parser/grammar/rule/SingleTokenRule.kt
@@ -0,0 +1,72 @@
+package compiler.parser.grammar.rule
+
+import compiler.lexer.Token
+import compiler.parser.TokenSequence
+import compiler.reportings.Reporting
+
+sealed class SingleTokenRule(
+    private val expectedToken: ExpectedToken,
+) : Rule<Token> {
+    override val explicitName = null
+
+    private val marksEndOfAmbiguityInContexts = HashSet<Any>()
+
+    final override val minimalMatchingSequence: Sequence<Sequence<ExpectedToken>> by lazy {
+        sequenceOf(sequenceOf(MarkingExpectedToken(expectedToken)))
+    }
+
+    final override fun tryMatch(context: Any, input: TokenSequence): RuleMatchingResult<Token> {
+        if (!input.hasNext()) {
+            return RuleMatchingResult(
+                isAmbiguous = true,
+                marksEndOfAmbiguity = false,
+                item = null,
+                reportings = setOf(Reporting.unexpectedEOI(descriptionOfAMatchingThing, input.currentSourceLocation))
+            )
+        }
+
+        input.mark()
+
+        val token = input.next()!!
+        val processed = matchAndPostprocess(token)
+        if (processed != null) {
+            input.commit()
+            return RuleMatchingResult(
+                isAmbiguous = false,
+                marksEndOfAmbiguity = context in marksEndOfAmbiguityInContexts,
+                item = processed,
+                reportings = emptySet(),
+            )
+        }
+
+        input.rollback()
+        return RuleMatchingResult(
+            isAmbiguous = true,
+            marksEndOfAmbiguity = false,
+            item = null,
+            setOf(Reporting.mismatch(descriptionOfAMatchingThing, token)),
+        )
+    }
+
+    override fun markAmbiguityResolved(inContext: Any) {
+        // nothing to do, no nested rules to inform and no own bookkeeping to adjust
+    }
+
+    /**
+     * @return if matched, the token, possibly modified (e.g. [IdentifierRule]). `null` on mismatch
+     */
+    abstract fun matchAndPostprocess(token: Token): Token?
+
+    override fun toString() = "single token: $descriptionOfAMatchingThing"
+
+    private inner class MarkingExpectedToken(
+        private val delegate: ExpectedToken,
+    ) : ExpectedToken {
+        override fun markAsRemovingAmbiguity(inContext: Any) {
+            marksEndOfAmbiguityInContexts.add(inContext)
+            delegate.markAsRemovingAmbiguity(inContext)
+        }
+        override fun unwrap() = delegate
+        override fun toString() = delegate.toString()
+    }
+}
