17fdfc06fc25ccebb7daf54cd9b01c0ff0fe75b7
Tobias <tobias.marstaller@gmail.com>
2024-02-08 18:04:47 +0100

    move getSupertypePointers and incrementStrongReferenceCount to core_intrinsic_functions.kt

--- llvm-backend/src/main/kotlin/io/github/tmarsteel/emerge/backend/llvm/intrinsics/core_intrinsic_functions.kt
+++ llvm-backend/src/main/kotlin/io/github/tmarsteel/emerge/backend/llvm/intrinsics/core_intrinsic_functions.kt
@@ -1,7 +1,12 @@
 package io.github.tmarsteel.emerge.backend.llvm.intrinsics
 
+import io.github.tmarsteel.emerge.backend.llvm.dsl.BasicBlockBuilder
 import io.github.tmarsteel.emerge.backend.llvm.dsl.BasicBlockBuilder.Companion.retVoid
+import io.github.tmarsteel.emerge.backend.llvm.dsl.GetElementPointerStep.Companion.member
 import io.github.tmarsteel.emerge.backend.llvm.dsl.KotlinLlvmFunction
+import io.github.tmarsteel.emerge.backend.llvm.dsl.LlvmContext
+import io.github.tmarsteel.emerge.backend.llvm.dsl.LlvmPointerType
+import io.github.tmarsteel.emerge.backend.llvm.dsl.LlvmValue
 import io.github.tmarsteel.emerge.backend.llvm.dsl.LlvmVoidType
 
 internal val valueArrayFinalize = KotlinLlvmFunction.define<EmergeLlvmContext, _>("valuearray_finalize", LlvmVoidType) {
@@ -18,4 +23,37 @@ internal val nullWeakReferences = KotlinLlvmFunction.define<EmergeLlvmContext, _
         // TODO: implement!
         retVoid()
     }
+}
+internal val getSupertypePointers = KotlinLlvmFunction.define<LlvmContext, _>(
+    "getSupertypePointers",
+    LlvmPointerType.pointerTo(EmergeArrayOfPointersToTypeInfoType),
+) {
+    val inputRef by param(PointerToAnyValue)
+    body {
+        val typeinfoPointer = getelementptr(inputRef)
+            .member { typeinfo }
+            .get()
+            .dereference()
+        val arrayPointer = getelementptr(typeinfoPointer)
+            .member { supertypes }
+            .get()
+            .dereference()
+
+        arrayPointer.incrementStrongReferenceCount()
+        return@body ret(arrayPointer)
+    }
+}
+
+context(BasicBlockBuilder<*, *>)
+internal fun LlvmValue<LlvmPointerType<out ArrayType<*>>>.incrementStrongReferenceCount() {
+    val referenceCountPtr = getelementptr(this@incrementStrongReferenceCount)
+        .member { base }
+        .member { anyBase }
+        .member { strongReferenceCount }
+        .get()
+
+    store(
+        add(referenceCountPtr.dereference(), word(1)),
+        referenceCountPtr,
+    )
 }
