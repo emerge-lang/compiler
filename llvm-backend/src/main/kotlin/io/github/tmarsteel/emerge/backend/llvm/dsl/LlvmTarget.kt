6c68ac0925f0f9d35951f32dfbdc8fa47bde100e
Tobias <tobias.marstaller@gmail.com>
2024-02-10 14:11:03 +0100

    add WIP LlvmTarget and LlvmTargetMachine

---
+++ llvm-backend/src/main/kotlin/io/github/tmarsteel/emerge/backend/llvm/dsl/LlvmTarget.kt
@@ -0,0 +1,37 @@
+package io.github.tmarsteel.emerge.backend.llvm.dsl
+
+import org.bytedeco.javacpp.BytePointer
+import org.bytedeco.javacpp.PointerPointer
+import org.bytedeco.llvm.LLVM.LLVMTargetRef
+import org.bytedeco.llvm.global.LLVM
+
+class LlvmTarget private constructor(
+    private val triple: String,
+    private val ref: LLVMTargetRef,
+) {
+    fun createTargetMachine(): LlvmTargetMachine {
+        val ref = LLVM.LLVMCreateTargetMachine(
+            ref,
+            triple,
+            "generic",
+            "",
+            LLVM.LLVMCodeGenLevelDefault,
+            RelocationModel.PIC.numeric,
+            CodeModel.SMALL.numeric
+        )
+        return LlvmTargetMachine(ref)
+    }
+
+    companion object {
+        fun fromTriple(triple: String): LlvmTarget {
+            val dataBuffer = PointerPointer<LLVMTargetRef>(1)
+            val errorBuffer = BytePointer(1024)
+            if (LLVM.LLVMGetTargetFromTriple(triple, dataBuffer, errorBuffer) != 0) {
+                throw IllegalArgumentException(errorBuffer.string)
+            }
+
+            val targetRef = dataBuffer.get(LLVMTargetRef::class.java, 0L)
+            return LlvmTarget(triple, targetRef)
+        }
+    }
+}
---
+++ llvm-backend/src/main/kotlin/io/github/tmarsteel/emerge/backend/llvm/dsl/LlvmTargetMachine.kt
@@ -0,0 +1,8 @@
+package io.github.tmarsteel.emerge.backend.llvm.dsl
+
+import org.bytedeco.llvm.LLVM.LLVMTargetMachineRef
+
+class LlvmTargetMachine(
+    val ref: LLVMTargetMachineRef
+) {
+}
