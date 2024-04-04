3459fa1069e13f12f9695f265b670a6d363d500f
Tobias Marstaller <tobias.marstaller@gmail.com>
2024-04-04 22:16:49 +0200

    frontend: improve error on import of undefined symbol

--- frontend/src/main/kotlin/compiler/reportings/UnresolvableImportReporting.kt
+++ frontend/src/main/kotlin/compiler/reportings/UnresolvableImportReporting.kt
@@ -7,7 +7,7 @@ data class UnresolvableImportReporting(
 ) : Reporting(
     Level.ERROR,
     "Could not find a function, type or variable with the name ${import.simpleName} in package ${import.packageName}",
-    import.declaration.declaredAt,
+    import.declaration.identifiers.last().sourceLocation,
 ) {
     override fun toString() = super.toString()
 }
