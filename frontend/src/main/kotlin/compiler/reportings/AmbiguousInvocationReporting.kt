89fd43b8addbf8946d18ea0812a98293a80b931b
Tobias Marstaller <tobias.marstaller@gmail.com>
2024-06-03 21:14:29 +0200

    fe: improve AmbiguousInvocationReporting be showing the alternatives considered

--- frontend/src/main/kotlin/compiler/reportings/AmbiguousInvocationReporting.kt
+++ frontend/src/main/kotlin/compiler/reportings/AmbiguousInvocationReporting.kt
@@ -11,6 +11,11 @@ class AmbiguousInvocationReporting(
     "Multiple overloads of ${candidates.first().name} apply to this invocation. Disambiguate by casting parameters explicitly.",
     invocation.span,
 ) {
+    override fun toString() = "$levelAndMessage\n${illustrateHints(
+        SourceHint(invocation.span, "this invocation is ambiguous", true),
+        *candidates.map { SourceHint(it.declaredAt, "this is a viable candidate", nLinesContext = 0u) }.toTypedArray(),
+    )}"
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
