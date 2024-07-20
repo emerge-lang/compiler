aedbb0ddac345c4ecf5bbad6fcab07eae982ddce
Tobias Marstaller <tobias.marstaller@gmail.com>
2024-07-20 16:49:15 +0200

    llvm: enforce assumptions in PhiBucket

--- llvm-backend/src/main/kotlin/io/github/tmarsteel/emerge/backend/llvm/dsl/PhiBucket.kt
+++ llvm-backend/src/main/kotlin/io/github/tmarsteel/emerge/backend/llvm/dsl/PhiBucket.kt
@@ -9,8 +9,15 @@ class PhiBucket<E : LlvmType>(val type: E) {
     private val branches = ArrayList<LlvmBasicBlockRef>()
     private val results = ArrayList<LlvmValueRef>()
 
+    private var built = false
+
     context(BasicBlockBuilder<*, *>)
     fun setBranchResult(result: LlvmValue<E>) {
+        check(!built) {
+            // is that actually true? Could be that just calling LLVMAddIncoming again will do the trick
+            // look into extending this code if you encounter this error
+            "phi node is already built, cannot add more branch results"
+        }
         assert(result.isLlvmAssignableTo(this.type))
         branches.add(Llvm.LLVMGetInsertBlock(builder))
         results.add(result.raw)
@@ -18,6 +25,17 @@ class PhiBucket<E : LlvmType>(val type: E) {
 
     context(BasicBlockBuilder<*, *>)
     fun buildPhi(): LlvmValue<E> {
+        check(!built) {
+            // that is likely possible, but not needed as of now
+            // look into extending this code if you encounter this error
+            "cannot build two phi nodes from the same PhiBucket"
+        }
+        built = true
+
+        check(branches.isNotEmpty()) {
+            "phi node with no incoming values"
+        }
+
         val phiInst = Llvm.LLVMBuildPhi(builder, type.getRawInContext(context), "result")
 
         NativePointerArray.fromJavaPointers(branches).use { incomingBranches ->
