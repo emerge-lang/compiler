5db387ca550bcc62a869d9d5b77ed556e21a9007
Tobias Marstaller <tobias.marstaller@gmail.com>
2024-08-27 14:36:20 +0200

    llvm: add test for the vtable algorithm

---
+++ llvm-backend/src/test/kotlin/io/github/tmarsteel/emerge/backend/llvm/KotestProjectConfig.kt
@@ -0,0 +1,40 @@
+package io.github.tmarsteel.emerge.backend.llvm
+
+import io.github.tmarsteel.emerge.backend.llvm.intrinsics.PointerToEmergeArrayOfPointersToTypeInfoType
+import io.github.tmarsteel.emerge.backend.llvm.jna.Llvm
+import io.kotest.core.config.AbstractProjectConfig
+import java.nio.file.Paths
+
+object KotestProjectConfig : AbstractProjectConfig() {
+    @Volatile
+    private var loaded = false
+    private val loadingMutex = Any()
+
+    override suspend fun beforeProject() {
+        PointerToEmergeArrayOfPointersToTypeInfoType
+        assureLlvmIsDynamicallyLinked()
+    }
+
+    private fun assureLlvmIsDynamicallyLinked() {
+        if (loaded) {
+            return
+        }
+        synchronized(loadingMutex) {
+            if (loaded) {
+                return
+            }
+
+            val propertyName = "emerge.llvm-tests.llvm-install-dir"
+            val installDirString = System.getProperty(propertyName)
+                ?: error("Missing system property $propertyName")
+            val installDir = Paths.get(installDirString)
+            loaded = true
+            Llvm.loadNativeLibrary(installDir)
+            Llvm.LLVMInitializeX86TargetInfo()
+            Llvm.LLVMInitializeX86Target()
+            Llvm.LLVMInitializeX86TargetMC()
+            Llvm.LLVMInitializeX86AsmPrinter()
+            Llvm.LLVMInitializeX86AsmParser()
+        }
+    }
+}
---
+++ llvm-backend/src/test/kotlin/io/github/tmarsteel/emerge/backend/llvm/VTableAlgorithmTest.kt
@@ -0,0 +1,176 @@
+package io.github.tmarsteel.emerge.backend.llvm
+
+import io.github.tmarsteel.emerge.backend.api.ir.IrBaseType
+import io.github.tmarsteel.emerge.backend.api.ir.IrClass
+import io.github.tmarsteel.emerge.backend.api.ir.IrCodeChunk
+import io.github.tmarsteel.emerge.backend.api.ir.IrExecutable
+import io.github.tmarsteel.emerge.backend.api.ir.IrFunction
+import io.github.tmarsteel.emerge.backend.api.ir.IrInterface
+import io.github.tmarsteel.emerge.backend.api.ir.IrMemberFunction
+import io.github.tmarsteel.emerge.backend.api.ir.IrOverloadGroup
+import io.github.tmarsteel.emerge.backend.api.ir.IrSourceFile
+import io.github.tmarsteel.emerge.backend.api.ir.IrSourceLocation
+import io.github.tmarsteel.emerge.backend.api.ir.IrTypeMutability
+import io.github.tmarsteel.emerge.backend.api.ir.IrVariableDeclaration
+import io.github.tmarsteel.emerge.backend.llvm.dsl.LlvmTarget
+import io.github.tmarsteel.emerge.backend.llvm.intrinsics.EmergeLlvmContext
+import io.github.tmarsteel.emerge.backend.llvm.intrinsics.buildVTable
+import io.github.tmarsteel.emerge.backend.llvm.intrinsics.missingVirtualFunctionHandler
+import io.github.tmarsteel.emerge.common.CanonicalElementName
+import io.kotest.core.spec.style.FreeSpec
+import io.mockk.mockk
+import java.nio.file.Paths
+import java.util.stream.Collectors.toMap
+import kotlin.random.Random
+
+/**
+ * This class is supposed to prove that the vtable algorithm is suitable for real-world use. C++ doesn't have issues
+ * there because it has one vtable per virtual type (which complicates casts extremely). Emerge only has one vtable
+ * per concrete type and needs to fit an arbitrary number of virtual types onto the same concrete type.
+ */
+class VTableAlgorithmTest : FreeSpec({
+    val packageName = CanonicalElementName.Package(listOf("vtabletest", randomIdentifier()))
+    for (nVirtualTypes in 1..10) {
+        "for $nVirtualTypes virtual types" - {
+            for (nVirtualFunctions in 1..30) {
+                "for $nVirtualFunctions virtual functions each" - {
+                    val virtualTypes: Set<IrInterface> = (1 until nVirtualTypes).map { typeN ->
+                        val typeName = CanonicalElementName.BaseType(packageName, "V$typeN")
+                        lateinit var typeHolder: IrInterface
+                        typeHolder = object : IrInterface {
+                            override val supertypes = emptySet<IrInterface>()
+                            override val canonicalName = typeName
+                            override val parameters = emptyList<IrBaseType.Parameter>()
+                            override val memberFunctions: List<IrOverloadGroup<IrMemberFunction>> = (1 until nVirtualFunctions).map { functionN ->
+                                SingletonIrOverloadGroup(object : IrMemberFunction {
+                                    override val canonicalName = CanonicalElementName.Function(
+                                        typeName,
+                                        "_v$functionN",
+                                    )
+                                    override val parameters = listOf(object : IrVariableDeclaration {
+                                        override val name = "self"
+                                        override val type by lazy { IrSimpleTypeImpl(typeHolder, IrTypeMutability.READONLY, false) }
+                                        override val isBorrowed = true
+                                        override val isReAssignable = false
+                                        override val isSSA = true
+                                    })
+                                    override val declaresReceiver = true
+                                    override val returnType = IrSimpleTypeImpl(MockIrUnit, IrTypeMutability.READONLY, false)
+                                    override val isNothrow = false
+                                    override val isExternalC = false
+                                    override val body: IrCodeChunk? = null
+                                    override val declaredAt = MockSourceLocation
+                                    override val ownerBaseType by lazy { typeHolder }
+                                    override val overrides = emptySet<IrMemberFunction>()
+                                    override val supportsDynamicDispatch = true
+                                })
+                            }
+                        }
+                        typeHolder
+                    }
+                        .toSet()
+
+                    lateinit var concreteType: IrClass
+                    concreteType = object : IrClass {
+                        override val supertypes: Set<IrInterface> = virtualTypes
+                        override val canonicalName = CanonicalElementName.BaseType(
+                            packageName,
+                            "Concrete"
+                        )
+                        override val parameters = emptyList<IrBaseType.Parameter>()
+                        override val memberFunctions = virtualTypes
+                            .flatMap { it.memberFunctions }
+                            .flatMap { it.overloads }
+                            .map { superFn ->
+                                SingletonIrOverloadGroup(object : IrMemberFunction {
+                                    override val canonicalName by lazy {
+                                        CanonicalElementName.Function(
+                                            concreteType.canonicalName,
+                                            superFn.canonicalName.simpleName,
+                                        )
+                                    }
+                                    override val parameters = superFn.parameters.mapIndexed { index, parameter ->
+                                        if (index > 0) {
+                                            return@mapIndexed parameter
+                                        }
+
+                                        object : IrVariableDeclaration {
+                                            override val name = parameter.name
+                                            override val type by lazy { IrSimpleTypeImpl(concreteType, parameter.type.mutability, parameter.type.isNullable) }
+                                            override val isBorrowed = parameter.isBorrowed
+                                            override val isReAssignable = parameter.isReAssignable
+                                            override val isSSA = parameter.isSSA
+                                        }
+                                    }
+                                    override val declaresReceiver = true
+                                    override val returnType = superFn.returnType
+                                    override val isNothrow = superFn.isNothrow
+                                    override val isExternalC = superFn.isExternalC
+                                    override val body: IrCodeChunk = object : IrCodeChunk {
+                                        override val components = emptyList<IrExecutable>()
+                                    }
+                                    override val declaredAt = MockSourceLocation
+                                    override val ownerBaseType by lazy { concreteType }
+                                    override val overrides = setOf(superFn)
+                                    override val supportsDynamicDispatch = true
+                                })
+                            }
+                        override val memberVariables = emptyList<IrClass.MemberVariable>()
+                        override val constructor = mockk<IrFunction>()
+                        override val destructor = mockk<IrFunction>()
+                    }
+
+                    EmergeLlvmContext.createDoAndDispose(LlvmTarget.fromTriple("x86_64-pc-linux-gnu")) { llvmCtx ->
+                        llvmCtx.registerBaseType(MockIrUnit)
+                        virtualTypes.forEach(llvmCtx::registerBaseType)
+                        llvmCtx.registerBaseType(concreteType)
+                        buildVTable(
+                            llvmCtx,
+                            concreteType.memberFunctions
+                                .flatMap { it.overloads }
+                                .flatMap { fn -> fn.signatureHashes.map { hash -> Pair(hash, fn) }}
+                                .onEach { llvmCtx.registerFunction(it.second) }
+                                .stream()
+                                .collect(toMap(
+                                    { it.first },
+                                    { it.second.llvmRef!! },
+                                )),
+                            llvmCtx.registerIntrinsic(missingVirtualFunctionHandler).address,
+                        )
+                    }
+                }
+            }
+        }
+    }
+})
+
+private fun randomIdentifier(): String {
+    val length = Random.nextInt(8, 12)
+    val chars = (0 .. length).map { ('a' .. 'z').random() }
+    return String(chars.toTypedArray().toCharArray())
+}
+private object MockSourceLocation : IrSourceLocation {
+    override val file = object : IrSourceFile {
+        override val path = Paths.get("/mock/file.em")
+    }
+    override val lineNumber: UInt = 1u
+    override val columnNumber: UInt = 1u
+}
+private abstract class MockCoreType(simpleName: String) : IrClass {
+    override val supertypes = emptySet<IrInterface>()
+    override val canonicalName = CanonicalElementName.BaseType(
+        CanonicalElementName.Package(listOf("emerge", "core")),
+        simpleName,
+    )
+    override val parameters = emptyList<IrBaseType.Parameter>()
+    override val memberFunctions = emptyList<IrOverloadGroup<IrMemberFunction>>()
+    override val memberVariables = emptyList<IrClass.MemberVariable>()
+    override val constructor: IrFunction = mockk()
+    override val destructor: IrFunction = mockk()
+}
+private object MockIrUnit : MockCoreType("Unit")
+private class SingletonIrOverloadGroup<T : IrFunction>(val single: T) : IrOverloadGroup<T> {
+    override val canonicalName by lazy { single.canonicalName }
+    override val parameterCount by lazy { single.parameters.size }
+    override val overloads = setOf(single)
+}
