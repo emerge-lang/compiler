ebdd57481d32943bc68ad668bef27b13c49765aa
Tobias Marstaller <tobias.marstaller@gmail.com>
2024-10-06 22:09:14 +0200

    Finalize  release workflows, second attempt

--- distributable/configure.sh
+++ distributable/configure.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-set -x
+set -e
 SELFDIR="$( realpath "$( dirname "${BASH_SOURCE[0]}" )" )"
 LLC="$(realpath "$(which llc-18)")"
 LLVM_DIR="${LLC%/bin/llc}"
 LLVM_DIR_FOR_SED=$(echo "$LLVM_DIR" | sed 's/\//\\\//g')
 
-sed --regexp-extended --in-place 's/llvm-installation-directory: \~/llvm-installation-directory: "'$LLVM_DIR_FOR_SED'"/' toolchain-config.yml
+sed --regexp-extended --in-place 's/llvm-installation-directory: \~/llvm-installation-directory: "'$LLVM_DIR_FOR_SED'"/' "$SELFDIR/toolchain-config.yml"
