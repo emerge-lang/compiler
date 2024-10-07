4fde9090fb77298e2e1e0a20d08ddfa22e27580b
Tobias Marstaller <tobias.marstaller@gmail.com>
2024-10-07 12:53:56 +0200

    restructure debian package files

--- dist/linux/all/configure.sh
+++ dist/linux/all/configure.sh
@@ -3,6 +3,7 @@ set -e
 SELFDIR="$( realpath "$( dirname "${BASH_SOURCE[0]}" )" )"
 LLC="$(realpath "$(which llc-18)")"
 LLVM_DIR="${LLC%/bin/llc}"
+echo "Using LLVM installation in $LLVM_DIR"
 LLVM_DIR_FOR_SED=$(echo "$LLVM_DIR" | sed 's/\//\\\//g')
 
 sed --regexp-extended --in-place 's/llvm-installation-directory: \~/llvm-installation-directory: "'$LLVM_DIR_FOR_SED'"/' "$SELFDIR/toolchain-config.yml"
